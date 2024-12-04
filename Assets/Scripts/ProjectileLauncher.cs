7e4e2443d21e4e8a927f73be41c8f592dc5495a1
Jacob Brockett <brockett04@gmail.com>
2024-12-03 22:17:23 -0600

    quick class cleanup

--- Assets/Scripts/ProjectileLauncher.cs
+++ Assets/Scripts/ProjectileLauncher.cs
@@ -14,19 +14,10 @@ public class ProjectileLauncher : MonoBehaviour
     [SerializeField] Transform spawnTransform;
     
     [Header("Ammo")]
-    [SerializeField] int maxAmmo = 10;
-    [SerializeField] int currentAmmo = 10;
-    [SerializeField] float maxReloadTime = 10f;
     [SerializeField] float coolDownTime = 0.25f;
 
     [SerializeField] float ammoDuration = 2f;
 
-    float currentReloadTime = 0;
-
-    void Awake()
-    {
-        currentAmmo = maxAmmo;
-    }
 
     bool coolingDown = false;
     // Launch a projectile Forward
@@ -37,8 +28,6 @@ public class ProjectileLauncher : MonoBehaviour
             return null;
         }
         CoolDown();
-
-        currentAmmo -= 1;
 
         GameObject newProjectile = Instantiate(projectilePrefab, spawnTransform.position, Quaternion.identity); // creates new projectile
 
@@ -57,8 +46,6 @@ public class ProjectileLauncher : MonoBehaviour
             return null;
         }
         CoolDown();
-
-        currentAmmo -= 1;
 
         GameObject newProjectile = Instantiate(projectilePrefab, spawnTransform.position, Quaternion.identity); // creates new projectile
 
@@ -81,58 +68,5 @@ public class ProjectileLauncher : MonoBehaviour
             coolingDown = false;
         }
     }
-
-    bool currentlyReloading = false;
-    public void Reload()
-    {
-        if (currentlyReloading){
-            return;
-        }
-        if (currentAmmo == maxAmmo)
-        {
-            return;
-        }
-
-        currentlyReloading = true;
-        currentReloadTime = 0;
-        StartCoroutine(ReloadRoutine());
-
-        IEnumerator ReloadRoutine()
-        {
-            Debug.Log("Reload routine active!");
-            // yield return new WaitForSeconds(reloadTime);
-
-            while(currentReloadTime < maxReloadTime)
-            {
-                yield return null; // wait for a single frame
-                currentReloadTime += Time.deltaTime;
-            }
-
-            currentReloadTime = 0;
-
-            currentAmmo = maxAmmo;
-            currentlyReloading = false;
-            Debug.Log("Reload routine done!");
-        }
-    }
-
-    public float GetReloadPercentage()
-    {
-        return currentReloadTime / maxReloadTime;
-    }
-
-    public int GetAmmo()
-    {
-        return currentAmmo;
-    }
-
-    public int GetMaxAmmo()
-    {
-        return maxAmmo;
-    }
-
-    public void SetProjectileSpeed(float projectileSpeed)
-    {
-        this.defaultProjectileSpeed = projectileSpeed;
-    }
+    
 }
