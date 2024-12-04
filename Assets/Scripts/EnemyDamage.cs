d6a32fd179a1307afc06672c83a31784b89a5828
Jacob Brockett <brockett04@gmail.com>
2024-12-03 23:13:57 -0600

    mosquito audio and health decrement audio

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class EnemyDamage : MonoBehaviour
 {
     [SerializeField] PlayerInputHandler playerInputHandler;
+    [Header("Audio")]
+    [SerializeField] AudioSource damageAudio;
     public void OnTriggerEnter2D(Collider2D other){
 
         if (other.CompareTag("Enemy")) // ensures object colliding is a bullet
         {
             playerInputHandler.DecrementHealth();
+
+            damageAudio.Play();
 
             Destroy(other.gameObject);
         }
